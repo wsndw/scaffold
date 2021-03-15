7e0eeebfb0c779bd5c22f69b28fb0ff6130ad533
wsndw <460858975@qq.com>
2021-03-15 23:02:59 +0800

    医院设置模块

---
+++ yygh_parent/service/service_hosp/src/main/java/cn/cq/yygh/hosp/mapper/HospitalSetMapper.java
@@ -0,0 +1,7 @@
+package cn.cq.yygh.hosp.mapper;
+
+import cn.cq.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+}
---
+++ yygh_parent/service/service_hosp/src/main/java/cn/cq/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.cq.yygh.hosp.service.impl;
+
+import cn.cq.yygh.hosp.mapper.HospitalSetMapper;
+import cn.cq.yygh.hosp.service.HospitalSetService;
+import cn.cq.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * yygh_parent
+ *
+ * @author : CQ
+ * @date : 2021-03-15 22:50
+ **/
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+
+
+}
